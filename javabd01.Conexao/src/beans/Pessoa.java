2c02c26a0f512eff9f1a2e29df1363cb17c3505b
Vicenzo de Souza <141375589+vitfbpa@users.noreply.github.com>
2024-10-25 11:22:12 -0300

    Add files via upload

---
+++ javabd01.Conexao/src/beans/Pessoa.java
@@ -0,0 +1,91 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package beans;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class Pessoa {
+    
+    private int id;
+    private String nome;
+    private String sexo;
+    private String idioma;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    } 
+    
+    public String toString() {
+        return this.id+" - "+this.nome;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 11 * hash + this.id;
+        hash = 11 * hash + Objects.hashCode(this.nome);
+        hash = 11 * hash + Objects.hashCode(this.sexo);
+        hash = 11 * hash + Objects.hashCode(this.idioma);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Pessoa other = (Pessoa) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.sexo, other.sexo)) {
+            return false;
+        }
+        return Objects.equals(this.idioma, other.idioma);
+    }
+    
+    
+}
---
+++ javabd01.Conexao/src/beans/Veiculo.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package beans;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class Veiculo {
+    private int id;
+    private String modelo;
+    private String placa;
+    private Pessoa pessoaid;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public Pessoa getPessoaid() {
+        return pessoaid;
+    }
+
+    public void setPessoaid(Pessoa pessoaid) {
+        this.pessoaid = pessoaid;
+    }
+}
